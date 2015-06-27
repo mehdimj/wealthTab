f16fefccbcd9260da6810d6b65a2e3ae51b7822f
mehdimj <mehdi.m.jamai@gmail.com>
2015-06-26 22:10:34 -0400

    Stacked bar Feeding data from mongodb collection

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,60 +1,44 @@
-Template.home.rendered = function() {
-       //ChartJS Example
-//    var tasks = Tasks.find().fetch();
-//    console.log(tasks);
-//    var names= [];
-//    var ages = [];
-//    tasks.forEach(function(data){
-//        names.push([data.name]);
-//        ages.push([data.age]);
-//    });
-//    console.log(names);
-//    console.log(ages);
-//    function drawChart(){
-//        var data = {
-//            type:'bar',
-//            labels : names,
-//            datasets : [
-//                {
-//                    fillColor : "rgba(220,220,220,0.5)",
-//                    strokeColor : "rgba(220,220,220,1)",
-//                    pointColor : "rgba(220,220,220,1)",
-//                    pointStrokeColor : "#fff",
-//                    data : ages
-//                }
-//            ]
-//        };
-//        var ctx = $("#myChart").get(0).getContext("2d");
-//        var myNewChart = new Chart(ctx);
-//        new Chart(ctx).Line(data);
-//    }
-//    drawChart();
-};
-//HighChart Example
-Template.highchart.rendered = function() {
-    var people = Tasks.find();
-    var names;
-    var ages = [];
-    var points = [];
-
-    people.forEach(function(data){
-        points.push([data.name],[data.age]);
+Tracker.autorun(function(){
+    Meteor.subscribe("tasks", function(){
     });
-    console.log(points);
-    chartOptions = {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: "bar",
-            renderTo: "highchart"
-        },
-        series: [{
-        type: 'bar',
-        name: 'WealthTab',
-        data: points
-    }]
-};
-
-var chart = new Highcharts.Chart(chartOptions);
+});
+Template.home.rendered = function() {
+    var duties = Tasks.find().fetch();
+    console.log(duties);
+    var result = [];
+    duties.forEach(function(data){
+        result.push({
+            name : data.name,
+            data : data.duties
+        })
+    });
+    console.log(result);
+    $(function(){
+        $('#container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Task Counter'
+            },
+            xAxis: {
+                categories: ['Clean', 'Work', 'Travel', 'Eat']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Assignee'
+                }
+            },
+            legend: {
+                reversed: true
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+            series: result
+        });
+    });
 };
