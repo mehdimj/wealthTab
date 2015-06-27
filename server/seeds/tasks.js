00117c396ef454450b4055efd17166a3a3e288bd
mehdimj <mehdi.m.jamai@gmail.com>
2015-06-27 08:02:59 -0400

    Update Seed

--- server/seeds/tasks.js
+++ server/seeds/tasks.js
@@ -2,11 +2,9 @@ Meteor.startup(function() {
     var arr = [];
     Factory.define('task', Tasks, {
         name: function() { return Fake.word(); },
-        data: [2,4,0,3]
+        data: function() {return [2,4,0,3]}
     });
-
     if (Tasks.find({}).count() === 0) {
-
         _(6).times(function(n) {
             Factory.create('task');
         });
